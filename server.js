b8fcac1bb5dc9ae89fd0a310715b3be1135eaebf
universla <ramirezrodriguezirma292@gmail.com>
2025-07-21 04:09:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 
 // server.js
-import express from 'express';
-import path from 'path';
-import { lowdb } from 'lowdb';
-import JSONFile from 'lowdb/adapters/JSONFile';
+const express = require('express');
+const path = require('path');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
 const app = express();
-const adapter = new JSONFile('db.json');
-const db = lowdb(adapter);
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
 // Inicializar base de datos
 db.defaults({ users: [], posts: [] }).write();
